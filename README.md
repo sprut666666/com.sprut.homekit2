fdaff59b8bbee2f772245d764ac0f68022f7bbb4
sprut666666 <sprut.1@me.com>
2017-10-11 18:04:16 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,111 @@
+# HomeKit for Homey
+
+![Image of paircode](https://github.com/sprut666666/com.sprut.homekit/raw/master/settings/code.png)
+
+After Homey is paired, go to "settings" -> "HomeKit". There select the devices you want to pair with "HomeKit" and wait =)
+Example of work: https://www.youtube.com/watch?v=yZWt6jDCl7E (New video from Homey the work)
+
+Remote access in HomeKit: https://support.apple.com/en-us/HT207057
+
+If you have problems update your "I" device & Apple TV. On iOS 11 everything works perfectly. If you don't see for example the "SPEAKER" look here https://itunes.apple.com/us/app/elgato-eve/id917695792?mt=8
+
+If the problem remained fully describe the situation. If you found any bugs, any other feature you can create an issue on [com.sprut.homekit](https://github.com/sprut666666/com.sprut.homekit)
+
+You can add any device and if it supported device types they will be added to HomeKit. If the device is not supported device types will be added to the device "NOT SUPPORTED" - If you want I added a new device type send me "full info:" the device from the log on sprut666666@gmail.com
+
+Now supports the types:
+- Light (On-off, dim, Temperature control, RGB)
+- Fan
+- Switch
+- Outlet
+- Door-lock
+- Curtains
+- Motion sensor
+- Humidity sensor
+- Light sensor
+- Carbon dioxide sensor
+- Temperature sensor
+- Leak sensor
+- Smoke sensor
+- Contact sensor (door/window sensor)
+- AirQuality sensor
+- Thermostat
+- Volume speaker
+- Vacuum cleaner
+- Button (simple and Play/Pause etc)
+- Doorbell button (as Motion sensor)
+
++ Battery service for all
+
+---
+
+### About
+Many thanks to the developer who wrote the library [has-node](https://github.com/abedinpour/HAS) Without which the application cannot run ;)
+Many thanks [abedinpour](https://github.com/abedinpour) so much for the work done.
+
+The basis of this application is taken development [com.swttt.homekit](https://github.com/swttt/com.swttt.homekit)
+Many thanks [Swttt](https://github.com/swttt) so much for the work done.
+
+And I [Sprut](https://github.com/sprut666666) - engaged in ongoing app development =)
+
+---
+
+### Changelog
+
+#### 2.0.2
+- update has-node 0.2.5
+
+#### 2.0.0
+- Big update (new logic for find devices)
+- Support MySensors + etc not standart plugins
+
+#### 1.1.9
+- Fix README
+
+#### 1.1.8
+- Fix info & README
+
+#### 1.1.7
+- If wakeup interval > 15 seconds - no online state
+
+#### 1.1.6
+- Extended support for status updates of devices
+
+#### 1.1.5
+- Verification of successful installation of the new parameters in Homey
+- Fix for MiLight
+
+#### 1.1.4
+- Small fixes
+
+#### 1.1.3
+- Critical bugfix when adding many devices
+
+#### 1.1.2
+- update has-node 0.2.3
+
+#### 1.1.1
+- fix WindowCovering
+
+#### 1.1.0
+- update has-node 0.2.2
+- new ColorTemperature
+- fix RGBW
+
+#### 1.0.5
+- fix bugs in Thermostat & expansion of functionality
+
+#### 1.0.4
+- fix Thermostat not measure_temperature
+
+#### 1.0.3
+- Add Doorbell button (as Motion sensor)
+
+#### 1.0.2
+- Add full device info for debug
+
+#### 1.0.1
+- Support 2 bridges for example com.swttt.homekit
+
+#### 1.0.0
+- Initial release
---
+++ app.js
@@ -0,0 +1,198 @@
+"use strict";
+
+const debug = true;
+
+// Enable TCP debug
+// process.env.DEBUG = 'TCP';
+
+const Homey = require('homey')
+const { HomeyAPI } = require('./lib/athom-api.js')
+const Homekit = require('./lib/homekit.js')
+
+
+
+let allDevices = {},
+    allPairedDevices = [],
+    server = {},
+    log = [];
+
+if (debug) {
+  console.log = function(string, type) {
+    const d = new Date();
+    const n = d.toLocaleTimeString();
+    let item = {};
+    item.time = n;
+    item.string = string;
+    item.type = type;
+    log.push(item);
+    Homey.ManagerApi.realtime('log.new', log)
+      .catch( this.error )
+    if(log.length > 50){
+      log.splice(0,1);
+    }
+  };
+}
+
+
+class HomekitApp extends Homey.App
+{
+  // Get homey object
+  getApi() {
+    if (!this.api) {
+      this.api = HomeyAPI.forCurrentHomey();
+    }
+    return this.api;
+  }
+  async getDevices() {
+    const api = await this.getApi();
+    allDevices = await api.devices.getDevices();
+    return allDevices;
+  }
+
+  getLog() {
+    return log;
+  }
+
+  // Start server function
+  async startingServer()
+  {
+    // Get the homey object
+    const api = await this.getApi();
+    // Get system info
+    const systeminfo = await api.system.getInfo();
+    // Subscribe to realtime events and set all devices global
+    await api.devices.subscribe();
+    allDevices = await api.devices.getDevices();
+
+    server = await Homekit.configServer(systeminfo);
+
+    // Loop all devices
+    allPairedDevices = await Homey.ManagerSettings.get('pairedDevices') || [];
+    let deviceForDel = [];
+
+    for (let i = 0; i < allPairedDevices.length; i++)
+    {
+      // If device has the class light
+      let device = allPairedDevices[i];
+
+      if (device.id in allDevices)
+      {
+        console.log(device.name + ' - device found.', 'info');
+        await this.addDevice(device,true);
+      }
+      else
+      {
+        console.log(device.name + ' - device not found.', 'info');
+        deviceForDel.push(device);
+      }
+    }
+
+    if (deviceForDel.length)
+    {
+      for (let i = 0; i < deviceForDel.length; i++)
+      {
+        let device = deviceForDel[i];
+
+        for (let i = 0; i < allPairedDevices.length; i++)
+        {
+          if (allPairedDevices[i] && allPairedDevices[i].id == device.id)
+          {
+            allPairedDevices.splice(i, 1);
+          }
+        }
+      }
+
+      await Homey.ManagerSettings.set('pairedDevices', allPairedDevices, (err, result) =>
+      {
+        if (err)
+          return Homey.alert(err);
+      });
+
+      console.log('Delete paired devices! => ' + deviceForDel.length, 'success');
+    }
+
+    if(allPairedDevices.length)
+    {
+      await console.log('Added all devices..done here!', 'success');
+    }
+    else{
+      await console.log('No devices found...', 'info');
+    }
+
+    // Start the server
+    await server.startServer();
+    console.log('Homekit server started.', 'success');
+
+    api.devices.on('device.delete', deviceID =>
+    {
+      allPairedDevices = Homey.ManagerSettings.get('pairedDevices') || [];
+
+      let deletePairedDevices = false;
+
+      for (let i = 0; i < allPairedDevices.length; i++)
+      {
+        if (allPairedDevices[i] && allPairedDevices[i].id == deviceID)
+        {
+          allPairedDevices.splice(i, 1);
+          deletePairedDevices = true;
+        }
+      }
+
+      if (deletePairedDevices)
+      {
+        server.removeAccessory(server.config.getHASID(deviceID));
+
+         Homey.ManagerSettings.set('pairedDevices', allPairedDevices, (err, result) =>
+        {
+          if (err)
+            return Homey.alert(err);
+        });
+
+        console.log('Delete device! => ' + deviceID, 'success');
+
+      }
+
+    });
+
+  }
+
+  async onInit()
+  {
+    // Start the server
+    await this.startingServer()
+      .then(console.log('Homekit server starting!', 'info'))
+      .catch(this.error);
+  }
+
+
+
+  async addDevice(device,noCheck)
+  {
+    if (noCheck === undefined)
+    {
+        await this.getDevices();
+        console.log(device.name + ' getDevices() ', 'info');
+    }
+
+    console.log(device.name + ' class: ' + allDevices[device.id].class, 'info');
+
+    let newDevice = await Homekit.createDevice(allDevices[device.id], server.config.getHASID(device.id));
+    await server.addAccessory(newDevice);
+
+    console.log(device.name + ' is added!', 'success');
+
+  }
+
+  async deleteDevice(device)
+  {
+    console.log('Trying to remove device ' + device.id, "info");
+
+    allDevices[device.id].removeAllListeners('$state');
+    server.removeAccessory(server.config.getHASID(device.id));
+
+    console.log(device.name + ' is removed!', 'success');
+  }
+
+}
+
+module.exports = HomekitApp
